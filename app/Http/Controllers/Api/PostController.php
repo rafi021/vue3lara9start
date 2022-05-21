1bc541dea8b963031225c2a0d9ea990b853c3d29
rafi021 <mahmud.ibrahim021@gmail.com>
2022-05-21 19:47:53 +0600

    Post Api Resource

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use PDO;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return Post::all();
+        return PostResource::collection(Post::all());
     }
 }
