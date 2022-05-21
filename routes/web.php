dfd0dd05c16edacdb910914354e7cff040721c3d
rafi021 <mahmud.ibrahim021@gmail.com>
2022-05-21 21:14:10 +0600

    fix Laravel route to match anything

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::view('/', 'dashboard')->name('dashboard');
+Route::view('/{any?}', 'dashboard')->name('dashboard')->where('any', '.*');
 
 // require __DIR__.'/auth.php';
