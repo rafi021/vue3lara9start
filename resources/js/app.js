c7f36184c9e4d5abb8b15ff22cd2cc7b37390361
rafi021 <mahmud.ibrahim021@gmail.com>
2022-05-21 18:13:09 +0600

    load first vue3 component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,8 @@
 require('./bootstrap');
 
+import { createApp } from 'vue';
+import PostIndex from './components/Posts/Index.vue'
+
+const app = createApp({});
+app.component('post-index', PostIndex);
+app.mount('#app');
