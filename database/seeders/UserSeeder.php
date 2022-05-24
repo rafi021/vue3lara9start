7f7e4a531ee9b68ce595c3fcfd9fe74afd073bad
rafi021 <mahmud.ibrahim021@gmail.com>
2022-05-24 20:10:15 +0600

    user created and update readme

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,8 +4,10 @@ namespace Database\Seeders;
 
 use App\Models\Role;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
 {
@@ -16,10 +18,22 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $admin = User::factory()->create();
+        $admin = User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make(1234),
+            'remember_token' => Str::random(10),
+        ]);
         $admin->roles()->attach(Role::where('name', 'Administrator')->value('id'));
 
-        $editor = User::factory()->create();
+        $editor = User::factory()->create([
+            'name' => 'editor',
+            'email' => 'editor@example.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make(1234),
+            'remember_token' => Str::random(10),
+        ]);
         $editor->roles()->attach(Role::where('name', 'Editor')->value('id'));
     }
 }
